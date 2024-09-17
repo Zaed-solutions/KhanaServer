e2632efe23d1af7c7a5c020ca2e34d84a2d4952c
Mohamed_Dev <mohamedzarea29@gmail.com>
2024-09-17 18:16:09 +0300

    feat/#2/implement_category_end_point: implement add category and get all categories end point

---
+++ src/main/kotlin/org/khana/khanaserver/data/entity/CategoryEntity.kt
@@ -0,0 +1,12 @@
+package org.khana.khanaserver.data.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "Category")
+data class CategoryEntity(
+    @Id
+    val id :String = "",
+    val categoryImage: String = "",
+    val categoryTitle: String = "",
+)
