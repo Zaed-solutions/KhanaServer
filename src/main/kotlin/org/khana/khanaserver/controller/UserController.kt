5d14bab528f52ec58c9392ed816b8da68ad416dc
Mohamed_Zarea <116445030+MoZarea@users.noreply.github.com>
2024-10-05 10:19:14 +0300

    Revert "Feat/#5/implement cart end point"

--- src/main/kotlin/org/khana/khanaserver/controller/UserController.kt
+++ src/main/kotlin/org/khana/khanaserver/controller/UserController.kt
@@ -8,6 +8,7 @@ import org.khana.khanaserver.service.model.UserDto
 import org.springframework.web.bind.annotation.*
 
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/users")
 class UserController(
