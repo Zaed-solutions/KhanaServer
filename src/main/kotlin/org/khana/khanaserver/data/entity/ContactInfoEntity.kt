90e0e37b77679ebcc15e387d3a8906d6ea8d12af
edrees <muhammed.edrees101@gmail.com>
2024-10-17 23:23:39 +0300

    feat/#33/implement_support_endpoints: Fixed a bug in inserting contact info

--- src/main/kotlin/org/khana/khanaserver/data/entity/ContactInfoEntity.kt
+++ src/main/kotlin/org/khana/khanaserver/data/entity/ContactInfoEntity.kt
@@ -1,9 +1,12 @@
 package org.khana.khanaserver.data.entity
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "contact_info")
 data class ContactInfoEntity(
+    @Id
+    val id: String = "",
     val customerSupportLines: List<String> = emptyList(),
     val whatsappNumbers: List<String> = emptyList(),
     val websiteUrls: List<String> = emptyList(),
