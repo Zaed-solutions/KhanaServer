7ec79b08316add007b01504f40115d9a22e12233
edrees <muhammed.edrees101@gmail.com>
2024-10-21 17:00:49 +0300

    feat/#1/implement_user_end_point: Implemented updateUserAvatar endpoint

--- src/main/kotlin/org/khana/khanaserver/controller/UserController.kt
+++ src/main/kotlin/org/khana/khanaserver/controller/UserController.kt
@@ -32,6 +32,13 @@ class UserController(
         return  result ;
     }
 
+    @PutMapping("/updateUserAvatar")
+    fun updateUserAvatar(@RequestParam userId: String, avatarUrl: String) = GenericResponse(
+        code = 200,
+        message = "Success",
+        data = userService.updateUserAvatar(userId, avatarUrl)
+    )
+
     @GetMapping("/byId")
     fun getUserById(@RequestParam id: String): GenericResponse<UserEntity> {
         println("id: $id")
