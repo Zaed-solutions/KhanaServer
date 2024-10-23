ab8cad5cd7d3b2d0b737167b1115e6da8018b4ed
edrees <muhammed.edrees101@gmail.com>
2024-10-23 21:20:40 +0300

    fix/#39/wishlisting_products_doesnt_work_properly: Fixed a bug related to WishlistedProductsEntity

--- src/main/kotlin/org/khana/khanaserver/data/entity/WishlistedProductsEntity.kt
+++ src/main/kotlin/org/khana/khanaserver/data/entity/WishlistedProductsEntity.kt
@@ -7,8 +7,8 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document(collection = "WishlistedProductsEntity")
 data class WishlistedProductsEntity(
     @Id val id: String? = null,
-    val user : UserEntity ,
-    val products : List<ProductEntity>,
+    val userId : String,
+    val productsIds : List<String>,
 )
 
 
